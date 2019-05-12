46df65a329132d7fe2b571e68a85b8c7452b1242
Bartosz Dempc <dempc.bartosz@gmail.com>
2019-05-12 12:20:50 +0200

    changed langugae code

--- autotaglibro/settings.py
+++ autotaglibro/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pl_PL'
+LANGUAGE_CODE = 'pl-PL'
 
 TIME_ZONE = 'UTC'
 
