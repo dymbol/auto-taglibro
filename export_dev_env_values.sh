c9b736cbd83a164e1378fa86615fb3cc82af9af0
Bartosz Dempc <dempc.bartosz@gmail.com>
2019-06-03 13:45:00 +0200

    changing sqlite dev database url

--- export_dev_env_values.sh
+++ export_dev_env_values.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export SECRET_KEY="Hjuioh78687^%586h98jh98"
-export DATABASE_URL="sqlite:////home/dymbol/tmp/autotaglibro/db.sqlite3;"
+export DATABASE_URL="sqlite:////autotaglibro/db.sqlite3;"
 export DEBUG="True"
 export DEV_ENV="True"
 export TelegramToken="598551213:AAHVT4vMsBt_IUCrdvMyDsoTOSD5NoM0o1A"
